5d8a47ec17ded01ba9ab10bccaba6d92aace23f8
makisho <makisho@gmail.com>
2016-08-02 00:09:39 +0200

    Fixed

--- src/app.js
+++ src/app.js
@@ -121,9 +121,7 @@ function chunkString(s, len) {
 }
 
 function sendFBMessage(sender, messageData, callback) {
-    alert('sendFBMessage');
-    
-    
+
     request({
         url: 'https://graph.facebook.com/v2.6/me/messages',
         qs: {access_token: FB_PAGE_ACCESS_TOKEN},
