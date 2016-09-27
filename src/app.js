ea2f8e0caa8a75ae84bfd2f33cfe9f13a8048a33
makisho <makisho@gmail.com>
2016-09-27 12:44:07 +0200

    s

--- src/app.js
+++ src/app.js
@@ -52,13 +52,13 @@ function processEvent(event) {
                 let FBimage = response.result.parameters.FBimage;
                 let FBvideo = response.result.parameters.FBvideo;
                 let FBbutton = response.result.parameters.FBbutton;
+                console.log(response.result.parameters.FBbutton);
                                
                 String.prototype.replaceAll = function(str1, str2, ignore) 
                 {
                     return this.replace(new RegExp(str1.replace(/([\/\,\!\\\^\$\{\}\[\]\(\)\.\*\+\?\|\<\>\-\&])/g,"\\$&"),(ignore?"gi":"g")),(typeof(str2)=="string")?str2.replace(/\$/g,"$$$$"):str2);
                 } 
                 
-               
                 
                 let action = response.result.action;
                 
